add78e1762290487c0f296abee72238a49de68ec
Pradnya <Pradnya@LAPTOP-AF1IOF6L>
2024-04-09 09:03:05 +0530

    Unichar Code Commit

---
+++ src/automationtestometer/UniChar.java
@@ -0,0 +1,13 @@
+package automationtestometer;
+
+public class UniChar {
+public static void main(String[] args) {
+	//Print your name in unicode (Hindi)
+	// ka='\u0915';
+//	System.out.println(ka);
+	
+	//write your name
+	String k1= "\u0906\u0915\u093E\u0936"; 
+	System.out.println(k1);
+}
+}
