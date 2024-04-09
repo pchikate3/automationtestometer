6c36f5e48843c4f2d30b03a056a6eef9ea6aae15
Pradnya <Pradnya@LAPTOP-AF1IOF6L>
2024-04-09 10:36:19 +0530

    Square Cube forth number commit

---
+++ src/automationtestometer/SquareCubeForthPower.java
@@ -0,0 +1,19 @@
+package automationtestometer;
+import java.util.Scanner;
+public class SquareCubeForthPower {
+	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+//		int a = scanner.nextInt();
+		float a1 = scanner.nextFloat();
+		
+//		System.out.println("Sqaure:" + a*a);
+//		System.out.println("Cube:" +a*a*a);
+//		System.out.println("forth power:" +a*a*a*a);
+//		
+		System.out.println("Sqaure:" + a1*a1);
+		System.out.println("Cube:" +a1*a1*a1);
+		System.out.println("forth power:" +a1*a1*a1*a1);
+	}
+}
